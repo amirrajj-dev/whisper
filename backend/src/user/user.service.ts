df2254ef0afffa932ab920ecdd4ce901a5170162
amirrajj <amiramraja@gmail.com>
2026-05-26 15:04:16 +0330

    added necessary auth needed business logic in user module

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDocument } from 'src/common/schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('User') private userModel: Model<UserDocument>) {}
+
+  findUserByEmail(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  findUserById(id: string): Promise<UserDocument | null> {
+    return this.userModel
+      .findById(id, '-password -__v')
+      .populate('blockedUsers', 'username email avatarUrl')
+      .exec();
+  }
+
+  createUser(data: {
+    email: string;
+    password: string;
+    username: string;
+  }): Promise<UserDocument> {
+    const newUser = new this.userModel(data);
+    return newUser.save();
+  }
+}
