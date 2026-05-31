1d8a06dd36b0ffeeebeacaf2b7b1eccde05504ce
amirrajj <amiramraja@gmail.com>
2026-05-31 10:58:36 +0330

    upload and delete from cloudinary logic wired in upload module

--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { CloudinaryService } from 'src/common/cloudinary/cloudinary.service';
 
 @Module({
-  providers: [UploadService],
-  controllers: [UploadController]
+  providers: [UploadService, CloudinaryService],
+  controllers: [UploadController],
+  exports: [UploadService],
 })
 export class UploadModule {}
--- backend/src/upload/upload.service.ts
+++ backend/src/upload/upload.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CloudinaryService } from 'src/common/cloudinary/cloudinary.service';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  constructor(private readonly cloudinaryService: CloudinaryService) {}
+
+  async uploadFile(
+    file: Express.Multer.File,
+    type: 'image' | 'file' | 'voice' | 'video',
+  ) {
+    return this.cloudinaryService.uploadFile(file, type);
+  }
+
+  async uploadMultipleFiles(
+    files: Express.Multer.File[],
+    type: 'image' | 'file' | 'voice' | 'video',
+  ) {
+    return this.cloudinaryService.uploadMultipleFiles(files, type);
+  }
+
+  async deleteFile(
+    publicId: string,
+    resourceType: 'image' | 'video' | 'raw' = 'image',
+  ) {
+    return this.cloudinaryService.deleteCloudinaryFile(publicId, resourceType);
+  }
+}
