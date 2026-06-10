1eb81959fc49bb7ac97ecd8111f0dd581460eb05
amirrajj <amiramraja@gmail.com>
2026-06-10 11:18:41 +0330

    nativewind setup

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -2,16 +2,8 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-indigo-600">
+      <Text className="text-white">Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
